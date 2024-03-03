ed7c6621407679e70f60ac967f997209f70eee96
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-03-03 15:36:23 +0100

    console.py

---
+++ console.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Console module"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """display prompt """
+    prompt = "(hbnb) "
+
+    def do_EOF(self, line):
+        """Exits the console when 'EOF' is entered"""
+        print()
+        return True
+
+    def do_quit(self, line):
+        """Exits the console when 'quit' is entered"""
+        return True
+
+    def help_quit(self, arg):
+        """Displays the help message for the 'quit' command"""
+        print("Quit command to exit the program")
+    
+    
+    
+if __name__ == '__main__':
+    """Initializes the command line interface and starts the loop"""
+    HBNBCommand().cmdloop()
