1794c67369245302e49c5e6fb757efb54f958383
Yassinelaz1 <yassinelazhari524@gmail.com>
2024-03-03 15:39:25 +0100

    console.py

--- console.py
+++ console.py
@@ -19,9 +19,13 @@ class HBNBCommand(cmd.Cmd):
     def help_quit(self, arg):
         """Displays the help message for the 'quit' command"""
         print("Quit command to exit the program")
-    
-    
-    
+
+    def emptyline(self):
+        """
+        do nothing when an empty line is entered.
+        """
+        pass
+
 if __name__ == '__main__':
     """Initializes the command line interface and starts the loop"""
     HBNBCommand().cmdloop()
